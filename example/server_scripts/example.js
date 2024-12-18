bd29b6ce7a6f9546243378c7417421708de833b1
westernat <westernat233@163.com>
2024-12-18 17:01:37 +0800

    版本

--- example/server_scripts/example.js
+++ example/server_scripts/example.js
@@ -7,8 +7,12 @@ FeatureEvents.onPlace('featurejs:normal_stone_spiral', event => {
     let topPos = context.level().getHeightmapPos('ocean_floor_wg', context.origin());
     let offset = Direction.NORTH;
     let height = context.config().getAsNumber(0);
-    for (let y = 0; y < height; y++) {
-        offset = offset.getClockWise();
-        context.level().setBlock(topPos.above(y).relative(offset), Blocks.STONE.defaultBlockState(), 3);
+    if (height < 0) {
+        event.setSucceed(false)
+    } else {
+        for (let y = 0; y < height; y++) {
+            offset = offset.getClockWise();
+            context.level().setBlock(topPos.above(y).relative(offset), Blocks.STONE.defaultBlockState(), 3);
+        }
     }
 })
